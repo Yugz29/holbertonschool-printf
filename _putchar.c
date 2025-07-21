b89a48a0819c6090b6424c852c00b4356ac1e97b
arsicbt <chobertarsinoe@gmail.com>
2025-07-21 15:22:24 +0000

    fix: rename _putchar.c

--- _putchar.c
+++ _putchar.c
@@ -2,23 +2,6 @@
 #include <unistd.h>
 
 /**
- * _strlen - Get the length of a string
- * @s: the string
- * Return: the length of the string 's'
-**/
-int _strlen(char *s)
-{
-	int len = 0;
-
-	while (s[len] != '\0')
-	{
-		len++;
-	}
-
-	return (len);
-}
-
-/**
  * _putchar - writes the character c to stdout
  * @c: The character to print
  *
