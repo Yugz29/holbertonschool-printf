b9a3856ad54252df1e27dfa6c2a42bb7b94a80d0
Yugz29 <yannduzelier@gmail.com>
2025-07-21 12:15:18 +0200

    feat : _putchar.c & MaJ _printf.c

--- _printf.c
+++ _printf.c
@@ -1,13 +1,26 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdarg.h>
 
 int _printf(const char *format, ...)
 {
 	int format_i = 0;
-	int type_i;
-	int len = _strlen(format);
+	int type_i, len;
 
-	while (format[format_i] < len)
+	if (format == NULL)
 	{
-		if (format[format_i])
+		return (-1);
+	}
+
+	len = _strlen(format);
+
+
+	while (format_i < len)
+	{
+		if (format[format_i] != '%')
+		{
+			_putchar(format[format_i]);
+		}
+		format_i++;
 	}
 }
--- main.h
+++ main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdarg.h>
+
 int _strlen(char *s);
 int _printf(const char *format, ...);
 
