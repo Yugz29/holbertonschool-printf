92f62f23d337577d0bf6270f9911f1aaef123983
Yugz29 <yannduzelier@gmail.com>
2025-07-23 17:03:20 +0200

    fix something in print_int

--- print_int.c
+++ print_int.c
@@ -1,22 +1,24 @@
 #include "main.h"
 
 /**
- * print_int - Print an intergers
+ * print_int - Print an interger
  * @argument_list: argument list of the varaidic function
- * Return: 1 value.
+ * Return: Number of caracteres printed
 **/
 int print_int(va_list *argument_list)
 {
 	int i;
+	int count = 0;
 
 	i = va_arg(*argument_list, int);
 
 	if (i < 0)
 	{
 		_putchar('-');
+		count++;
 		i *= -1;
 	}
 
-	_putchar((i / 10) + '0');
-	return (1);
+	count += print_number_recursive(i);
+	return (count);
 }
