e79a3d885e71735b50995eee6e53f95f02b955aa
arsicbt <chobertarsinoe@gmail.com>
2025-07-23 14:35:00 +0000

    fix: try with 2 files, for the intranet's checker

--- print_dec.c
+++ print_dec.c
@@ -1,21 +1,6 @@
 #include "main.h"
 
 /**
- * print_rec - Recursively prints an integers number
- * @x: the number to print
- * Return: number of charactere printed
-**/
-int print_rec(int x)
-{
-	if (x >= 10)
-	{
-		print_rec(x / 10);
-	}
-
-	return (_putchar((x % 10) + '0'));
-}
-
-/**
  * print_dec - Print a signed decimal integer
  * @argument_list: pointer to the list of argument
  * Return: number of charactere printed.
